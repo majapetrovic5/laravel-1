411405f8dadd8c6bfd2c1934300397851be7db79
majapetrovic5 <76411291+majapetrovic5@users.noreply.github.com>
2022-01-03 16:35:33 +0100

    Ogranicenja za PhoneNumber i promena tipa u Reports

---
+++ laravel-domaci/database/migrations/2022_01_03_152112_change_to_foreign_id_in_table_reports.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeToForeignIdInTableReports extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->foreignId('patientId')->change();
+            $table->foreignId('doctorId')->change(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->integer('patientId');
+            $table->integer('doctorId'); 
+        });
+    }
+}
