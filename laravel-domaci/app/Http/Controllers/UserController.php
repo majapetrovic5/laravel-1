884e205c5b80520edfc43bec397b7ebdc9dd38bb
majapetrovic5 <76411291+majapetrovic5@users.noreply.github.com>
2022-01-05 21:31:07 +0100

    Definisane metode update() i destroy()

--- laravel-domaci/app/Http/Controllers/UserController.php
+++ laravel-domaci/app/Http/Controllers/UserController.php
@@ -9,6 +9,9 @@ use \App\Models\PatientStatus;
 use \App\Models\Report;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -46,12 +49,12 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User $user
+     * @param  \App\Models\User $doctor
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user)
+    public function show(User $doctor)
     {
-        return new UserResource($user);
+        return new UserResource($doctor);
     }
 
     /**
@@ -72,9 +75,31 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $doctor)
     {
-        //
+        if(Auth()->user()->isAdmin() || Auth()->user()->id === $doctor->id) {
+       
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:100',
+                'email' => 'required|string|max:50|email|unique:users,email,'.$doctor->id,
+                'password' => 'required|string|regex:"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"',
+            ]);
+    
+            if ($validator->fails())
+                return response()->json($validator->errors());
+    
+          // $user->remember_token = Str::random(10);
+          $doctor->name = $request->name;
+          $doctor->email = $request->email;
+          $doctor->password = Hash::make($request->password);
+          $doctor->email_verified_at = now();
+          $doctor->save();
+           return response()->json(['Successfully updated doctor', 'data' => $doctor]);
+        
+            //return response()->json('Successfully registered doctor.');
+    
+        }
+        else return response()->json('Unauthorized to update other doctors.');
     }
 
     /**
@@ -83,8 +108,21 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $doctor)
     {
-        //
+        if(Auth()->user()->isAdmin()){
+        if($doctor->id != Auth()->user()->id){
+            $doctor->delete();
+            return response()->json('Doctor is deleted successfully.');
+        }
+        else return response()->json('You cannot delete admin (yourself)!');
+    } 
+    return response()->json('You are not authorized to delete doctors.');
+}
+
+    public function showme(){
+ 
+          return new UserResource(Auth()->user());
+
     }
 }
